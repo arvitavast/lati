de2cee73a8a9e974b0c4501fddaaea9b0ed31cf5
Arvi Tavast <arvi@tavast.ee>
2014-11-14 16:49:15 +0100

    lisan vähem

--- stats.py
+++ stats.py
@@ -43,7 +43,7 @@ def nowords(sourcelang="lv"):
     cursor.execute(comparequery) #, (sourcelang, ))
     result = cursor.fetchall()
 
-    tk = 3220
+    tk = 3200
 
     moisteid = result[0][2] + tk
     eestiterm = result[0][0] + tk
